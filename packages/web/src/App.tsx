f8222376b913396b55cd318b1115e6e7fcd00c32
zeus0123 <37953384+zeus0123@users.noreply.github.com>
2021-12-19 17:42:47 +0530

    sample mono repo setup

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@zeus0123/common';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button textColor="blue">Hello, Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
